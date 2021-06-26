4df7dacbd2a8deceb60ff06e438c9059aac10599
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-26 21:07:17 +0200

    ajout animation socials

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.addEventListener('DOMContentLoaded', (_event) => {
-    // new TimelineLite()
-    //     .from('.footer-left', 1, { x: -200, opacity: 0 })
-    //     .from('.footer-right', 1, { x: 200, opacity: 0 }, '-=1')
-    //     .from('.letter-image', 0.5, { y: -150, opacity: 0 });
-    
     AOS.init();
+
+    document.querySelectorAll('.socials i').forEach((icon, key) => {
+        new TimelineLite().from(icon, 1 + key, { x: -500 * (key + 1), opacity: 0 }, '+=0.5');
+    });
 });
