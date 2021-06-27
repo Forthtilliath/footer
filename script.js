cf6bf3d2464db2726325608b10c8758af0545254
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-06-27 03:18:20 +0200

    animations

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ window.addEventListener('DOMContentLoaded', (_event) => {
     document.querySelectorAll('.socials i').forEach((icon, key) => {
         new TimelineLite().from(icon, 1 + key, { x: -500 * (key + 1), opacity: 0 }, '+=0.5');
     });
+
+    let xScreen = window.innerWidth;
+    const duration = 4;
+    window.addEventListener('resize', () => (xScreen = window.innerWidth));
+
+    let TL_formulaOne_modal = new TimelineLite({ paused: true })
+        .fromTo('.formula-red', duration, { x: xScreen * -2.5 }, { x: xScreen * 2.5 })
+        // .fromTo('.curve', duration, { x: xScreen * -2.5 }, { x: xScreen * 2.5 }, '-=' + duration)
+        .fromTo('.modal-content', duration, { x: xScreen * -4.5, opacity: 1 }, { x: 0, opacity: 1 }, '-=' + duration);
+    let TL_formulaOne_modal_out;
+    
+    document.querySelector('.letter').addEventListener('click', () => {
+        TL_formulaOne_modal.restart();
+        TL_formulaOne_modal_out = new TimelineLite({ paused: true })
+            .fromTo('.modal-content', duration / 2, { x: 0, opacity: 1 }, { x: xScreen * 2, opacity: 0 });
+    });
+
+    document.querySelector('.modal-close').addEventListener('click', () => {
+        TL_formulaOne_modal_out.play();
+    });
 });
